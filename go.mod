cb072a1429308c06ac2626a5a2ad83079533cc06
Somesh Kar <somesh.kar@gmail.com>
2020-05-21 13:27:24 +0530

    Test goroutines

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/someshkar/whois-api
 go 1.14
 
 require (
+	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/likexian/whois-go v1.6.0
 	github.com/likexian/whois-parser-go v1.14.2
 )
--- lib/whois.go
+++ lib/whois.go
@@ -19,3 +19,22 @@ func GetWhois(domain string) (whoisparser.WhoisInfo, error) {
 
 	return result, nil
 }
+
+// GetChanWhois sends Whois data to a channel
+func GetChanWhois(domain string, whoisCh chan<- whoisparser.WhoisInfo, errorCh chan<- error) {
+	raw, err := whois.Whois(domain)
+	if err != nil {
+		// return whoisparser.WhoisInfo{}, err
+		whoisCh <- whoisparser.WhoisInfo{}
+		errorCh <- err
+	}
+
+	result, err := whoisparser.Parse(raw)
+	if err != nil {
+		// return whoisparser.WhoisInfo{}, err
+		whoisCh <- whoisparser.WhoisInfo{}
+		errorCh <- err
+	}
+
+	whoisCh <- result
+}
